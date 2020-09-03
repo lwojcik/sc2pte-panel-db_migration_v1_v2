2ec34ce901446d8e37785a9563e50050316592eb
Łukasz Wójcik <lwojcik@lukem.net>
2020-09-03 13:30:53 +0200

    final revision

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const LegacyChannelConfigSchema = require('./models/LegacyChannelConfig');
-// const ChannelConfigV2 = require('./models/ChannelConfigV2');
+const ChannelConfigSchema = require('./models/ChannelConfig');
 
 mongoose.connection.on('error', (err) => {
   console.error(`MongoDB error: ${err}`);
@@ -41,13 +41,52 @@ const connectToDb = (connectionString) =>
     },
   );
 
-const iterateDocuments = async () => {
-  const oldDb = connectToDb(V1_DATABASE_CONNECTION_STRING);
-
-  const oldConfigs = oldDb.model('ChannelConfig', LegacyChannelConfigSchema);
-  const data = await oldConfigs.find({});
-  console.log(data);
-  process.exit(0);
+const saveNewConfig = async (NewConfig, legacyConfigObject) => {
+  const newConfigObject = new NewConfig({
+    channelId: legacyConfigObject.channelId,
+    createdAt: legacyConfigObject.createdAt,
+    updatedAt: legacyConfigObject.updatedAt,
+    profiles: [
+      {
+        regionId: legacyConfigObject.regionId,
+        realmId: legacyConfigObject.realmId,
+        profileId: legacyConfigObject.playerId,
+        locale: 'en-us',
+      },
+    ]
+  });
+  console.log(`Saving document: ${legacyConfigObject.channelId}`);
+  await newConfigObject.save();
 }
 
-iterateDocuments();
+const start = async () => {
+  console.log('Starting...');
+  console.log('Connecting to old db...');
+  const oldDb = await connectToDb(V1_DATABASE_CONNECTION_STRING);
+  const LegacyConfigs = oldDb.model('ChannelConfig', LegacyChannelConfigSchema); 
+  console.log('Old db connected!');
+
+  console.log('Connecting to new db...');
+  const newDb = await connectToDb(V2_DATABASE_CONNECTION_STRING);
+  const NewConfig = newDb.model('ChannelConfig', ChannelConfigSchema); 
+  console.log('New db connected!');
+
+  // cleaning new database before the operation to avoid duplicate id errors
+  console.log('Cleaning new Db...')
+  await NewConfig.deleteMany({});
+
+  let i = 0;
+
+  // here we goooooooo
+  for await (const legacyConfig of LegacyConfigs.find()) {
+    await saveNewConfig(NewConfig, legacyConfig);
+    i++;
+    console.log(`Documents processed: ${i}`);
+  }
+}
+
+(async function() {
+  await start();
+  console.log('Done!');
+  process.exit();
+})();
